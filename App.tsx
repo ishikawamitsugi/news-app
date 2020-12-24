c0664f8a400f11dad314d1bdb1a2fef04e6ef8c1
ishikawamitsugi <ishikawamitsugi@gmail.com>
2020-12-24 14:02:01 +0900

    app.tsxに記事の見出しを表示する内容を記述した

--- App.tsx
+++ App.tsx
@@ -1,18 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          height: 100,
-          width: "100%",
-          borderColor: "gray",
-          borderWidth: 1,
-        }}
-      />
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            source={{
+              uri: "https://picsum.photos/id/10/200/200/",
+            }}
+          ></Image>
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text} numberOfLines={3}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Text>
+          <Text style={styles.subText}>ReactNews</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -23,5 +37,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray",
   },
 });
