710d6e5f28b518020ba57e0916078e54b08a3048
ishikawamitsugi <ishikawamitsugi@gmail.com>
2020-12-24 23:00:49 +0900

    API経由で記事を表示するように変更した

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList, SafeAreaView, Platform } from "react-native";
 import ListItem from "./components/ListItem";
-import articles from "./dummies/articles.json";
+import dummyArticles from "./dummies/articles.json";
 import Constants from "expo-constants";
+import axios, { AxiosResponse } from "axios";
 
 const styles = StyleSheet.create({
   droidSafeArea: {
@@ -13,6 +14,27 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  var url =
+    "http://newsapi.org/v2/top-headlines?" +
+    "country=jp&" +
+    `apiKey=${Constants.manifest.extra.newsApiKey}`;
+  const [articles, setArticles] = useState<any>([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(Constants.manifest.extra.newApiKey);
+    fetchArticles();
+  }, []);
+
   return (
     <SafeAreaView style={styles.droidSafeArea}>
       <FlatList
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -42,15 +42,17 @@ const ListItem: React.FC<LIstItemProps> = ({ author, imageUrl, title }) => {
   return (
     <View style={styles.itemContainer}>
       <View style={styles.leftContainer}>
-        <Image
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-          source={{
-            uri: imageUrl,
-          }}
-        ></Image>
+        {!!imageUrl && (
+          <Image
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            source={{
+              uri: imageUrl,
+            }}
+          />
+        )}
       </View>
       <View style={styles.rightContainer}>
         <Text style={styles.text} numberOfLines={3}>
