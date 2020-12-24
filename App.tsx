d9b395e188aa82d8aa32a9170031dde670cb5e6a
ishikawamitsugi <ishikawamitsugi@gmail.com>
2020-12-24 20:03:07 +0900

    ListItemを作成してダミーニュース記事の一覧表示ができるようにした。

--- App.tsx
+++ App.tsx
@@ -1,63 +1,32 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, FlatList, SafeAreaView, Platform } from "react-native";
+import ListItem from "./components/ListItem";
+import articles from "./dummies/articles.json";
+
+const styles = StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-            source={{
-              uri: "https://picsum.photos/id/10/200/200/",
-            }}
-          ></Image>
-        </View>
-        <View style={styles.rightContainer}>
-          <Text style={styles.text} numberOfLines={3}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </Text>
-          <Text style={styles.subText}>ReactNews</Text>
-        </View>
-      </View>
-    </View>
+    <SafeAreaView style={styles.droidSafeArea}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            imageUrl={item.urlToImage}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => {
+          return index.toString();
+        }}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    borderColor: "gray",
-    borderWidth: 1,
-    flexDirection: "row",
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between",
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray",
-  },
-});
