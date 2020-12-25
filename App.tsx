a6cedafe1c63cd0edca22c014d82803adc44e84d
ishikawamitsugi <ishikawamitsugi@gmail.com>
2020-12-25 11:37:58 +0900

    safeAreaViewを各コンポーネントで設定するように修正

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Platform } from "react-native";
 import AppNavigator from "./Navigation/AppNavigator";
 
-const styles = StyleSheet.create({
-  droidSafeArea: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? 25 : 0,
-  },
-});
-
 export default function App() {
-  return (
-    <SafeAreaView style={styles.droidSafeArea}>
-      <AppNavigator />
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,6 +4,14 @@ import ListItem from "../components/ListItem";
 import Constants from "expo-constants";
 import axios from "axios";
 
+const styles = StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+});
+
 const HomeScreen: React.FC<any> = ({ navigation }) => {
   var url =
     "http://newsapi.org/v2/top-headlines?" +
@@ -25,20 +33,22 @@ const HomeScreen: React.FC<any> = ({ navigation }) => {
   }, []);
 
   return (
-    <FlatList
-      data={articles}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          imageUrl={item.urlToImage}
-          author={item.author}
-          onPress={() => navigation.navigate("Article")}
-        />
-      )}
-      keyExtractor={(item, index) => {
-        return index.toString();
-      }}
-    ></FlatList>
+    <SafeAreaView style={styles.droidSafeArea}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            imageUrl={item.urlToImage}
+            author={item.author}
+            onPress={() => navigation.navigate("Article")}
+          />
+        )}
+        keyExtractor={(item, index) => {
+          return index.toString();
+        }}
+      ></FlatList>
+    </SafeAreaView>
   );
 };
 
