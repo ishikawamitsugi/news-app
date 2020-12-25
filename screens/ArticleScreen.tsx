0ed04b45234af1010c58c7543630e2e52382cf53
ishikawamitsugi <ishikawamitsugi@gmail.com>
2020-12-25 11:34:15 +0900

    WebViewを導入して遷移先でダミーのページを表示するようにした

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export const ArticleScreen: React.FC = () => {
   return (
-    <View>
-      <Text>Hello, React!</Text>
-    </View>
+    <WebView source={{ uri: "https://expo.io" }} style={{ marginTop: 20 }} />
   );
 };
 export default ArticleScreen;
