46fe2b4fb726b348dcd0e97357fe34ada6637467
ishikawamitsugi <ishikawamitsugi@gmail.com>
2020-12-28 18:44:05 +0900

    ダミーのボタン押下時にReduxのStoreに保存・削除するようにした

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,11 +1,54 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  AppState,
+} from "react-native";
 import { WebView } from "react-native-webview";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addClip,
+  ADD_CLIP,
+  deleteClip,
+  DELETE_CLIP,
+} from "../store/actions/user";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
 export const ArticleScreen: React.FC<any> = ({ route }) => {
   // routeから渡したarticleをここで取得する
   const { article } = route.params;
-  console.log(article);
-  return <WebView source={{ uri: article.url }} style={{ marginTop: 20 }} />;
+  const dispatch = useDispatch();
+  const clip = useSelector((state) => state);
+  console.log("ReduxState: ", clip);
+
+  return (
+    <React.Fragment>
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity
+          onPress={(): void => {
+            dispatch(addClip({ type: ADD_CLIP, article }));
+          }}
+        >
+          <Text style={{ marginTop: 15, fontSize: 30 }}>Add_Clip</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={(): void => {
+            dispatch(deleteClip({ type: DELETE_CLIP, article }));
+          }}
+        >
+          <Text style={{ marginTop: 5, fontSize: 30 }}>DELETE_Clip</Text>
+        </TouchableOpacity>
+        <WebView source={{ uri: article.url }} />
+      </SafeAreaView>
+    </React.Fragment>
+  );
 };
 export default ArticleScreen;
--- store/actions/user.ts
+++ store/actions/user.ts
@@ -2,14 +2,14 @@ export const ADD_CLIP = 'ADD_CLIP';
 export const DELETE_CLIP = 'DELETE_CLIP';
 
 
-export const AddClip = (clip:any) => {
+export const addClip = (clip:any) => {
     return {
         type: ADD_CLIP,
         clip: clip
     }
 }
 
-export const DeleteClip = (clip: any) => {
+export const deleteClip = (clip: any) => {
     return {
         type: DELETE_CLIP,
         clip: clip
